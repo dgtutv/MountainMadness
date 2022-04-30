fec20fe02fd85c9fe2497be3e7f42547dadf62bc
RastkoAmun <raciosh@gmail.com>
2022-04-30 11:47:50 -0700

    Updated version of fronend, please use this version

--- hackathon/src/Opening.js
+++ hackathon/src/Opening.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./Opening.css"
+import MainPage from './MainPage';
+
+function startTheGame(){
+  return
+}
 
 export default function Opening() {
   return (
     <div className="Opening">
-        <button className='start-button'> START </button>
+        <button className='start-button' onClick={startTheGame}> START </button>
     </div>
   )
 }
