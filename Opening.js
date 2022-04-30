d0d7576133d48fd7945098027e01ee0c61a46fb6
RastkoAmun <raciosh@gmail.com>
2022-04-29 23:16:06 -0700

    Opening page for Mohammed

---
+++ Opening.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Opening.css"
+
+export default function Opening() {
+  return (
+    <div className="Opening">
+        <button className='start-button'> START </button>
+    </div>
+  )
+}
+
