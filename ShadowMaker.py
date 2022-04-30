040ac7e3d2d16c0eb01729a927381c4fc53d2a5f
Daniel <dgt1@sfu.ca>
2022-04-29 22:28:27 -0700

    Somewhat works

--- ShadowMaker.py
+++ ShadowMaker.py
@@ -6,6 +6,7 @@ for subdirectory, directory, images in os.walk(root):
     for image in images:
         print(image)
         currImage = Image.open(subdirectory+"\\"+image)
+        currImage = currImage.convert("RGBA")
         gray = currImage.convert('L')
         bw = gray.point(lambda x: 0 if x!=255 else 255, '1')
         bw.save(subdirectory+"\\"+"dark-"+image)
