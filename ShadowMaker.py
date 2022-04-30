9ed7036230823d9b87d179a9d42b5b6867a2c86c
Daniel <dgt1@sfu.ca>
2022-04-29 21:56:16 -0700

    progress on black and white

--- ShadowMaker.py
+++ ShadowMaker.py
@@ -6,6 +6,6 @@ for subdirectory, directory, images in os.walk(root):
     for image in images:
         print(image)
         currImage = Image.open(subdirectory+"\\"+image)
-        modifier = ImageEnhance.Brightness(currImage)
-        output = modifier.enhance(0)
-        output.save("dark-"+image)
+        gray = currImage.convert('L')
+        bw = gray.point(lambda x: 0 if x<128 else 255, '1')
+        bw.save(subdirectory+"\\"+"dark-"+image)
