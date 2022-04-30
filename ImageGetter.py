ff15fed7fe3febbaca5c0588d8bab1d0ed74e2c4
Daniel <dgt1@sfu.ca>
2022-04-30 04:19:16 -0700

    Shadows and shit

--- ImageGetter.py
+++ ImageGetter.py
@@ -2,28 +2,33 @@ from bing_image_downloader import downloader
 import os
 
 #deletes images that are jpegs, also returns number of jpegs found
-def jpegRemove():
+def jpegRemove(query):
     root = "imagesROOT"
+    deleteRoot = "imagesROOT\\"+query
     counter = 0
     for subdirectory, directory, images in os.walk(root):
-        for image in images:
-            extention = image[len(image)-4:]
-            if extention == ".jpg":
-                counter+=1
-                removeString = 'imagesROOT\\'+ image
-                os.remove(removeString)
+        if subdirectory == deleteRoot:
+            for image in images:
+                extention = image[len(image)-4:]
+                if extention == ".jpg":
+                    counter+=1
+                    os.remove(deleteRoot+"\\"+image)
+        
     return counter
 
 #filters through 
-def filter():
-    while(jpegRemove()!=0):
-        downloader.download(query, limit=num+counter,  output_dir="bin", adult_filter_off=False, force_replace=False, timeout=60, filter="transparent", verbose=True)
+def filter(query):
+    while(True):
+        result = jpegRemove(query)
+        if(result==0): 
+            return
+        downloader.download(query, limit=num+result,  output_dir="bin", adult_filter_off=False, force_replace=False, timeout=60, filter="transparent", verbose=True)
         root = "bin"
         i = 0
         for subdirectory, directory, images in os.walk(root):
             for image in images:
                 i+=1
-                if i<counter:
+                if i<result:
                     os.remove(image)
 
 #main program
@@ -36,4 +41,4 @@ while(cont):
     query = input("What do you want images of?   ")
     num = int(input("How many images of "+ query + " do you want?   "))
     downloader.download(query, limit=num,  output_dir="imagesROOT", adult_filter_off=False, force_replace=False, timeout=60, filter="transparent", verbose=True) 
-    filter()
+    filter(query)
